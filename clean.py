a809898b79d92a31b6e8e294a62f3cbcf0c0091a
velansalis <velanmax8@gmail.com>
2019-04-18 13:11:56 +0530

    Added comments

--- clean.py
+++ clean.py
@@ -3,8 +3,18 @@ import cv2 as opencv
 import numpy as np
 import os
 
-def clean_data(labels_path, labels_name):
-    with open("%s/%s" % (labels_path, labels_name)) as labels:
+pathinfo = dict()
+pathinfo["consonent_path"] = 'data/devanagari-character-dataset/nhcd/consonants'
+pathinfo["numerals_path"] = 'data/devanagari-character-dataset/nhcd/numerals'
+pathinfo["vowels_path"] = 'data/devanagari-character-dataset/nhcd/vowels'
+pathinfo["labels_path"] = 'data/devanagari-character-dataset/labels.csv'
+
+def clean_data(labels_path):
+    '''
+    This method will import labels.csv from the dataset and retreive the clean label classes
+    and return them as a dictionary
+    '''
+    with open("%s" % (labels_path)) as labels:
         content = [value.replace(",,,", "")
                    for value in labels.read().split("\n") if value != ",,,"]
         clean_data = {}
@@ -14,29 +24,24 @@ def clean_data(labels_path, labels_name):
         clean_data["consonants"] = content[28:64]
         return clean_data
 
-consonent_path = 'data/devanagari-character-dataset/nhcd/consonants'
-numerals_path = 'data/devanagari-character-dataset/nhcd/numerals'
-vowels_path = 'data/devanagari-character-dataset/nhcd/vowels'
-labels_path = 'data/devanagari-character-dataset'
-labels_name = 'labels.csv'
-
 def convert_to_npy():
-    newlist = clean_data(labels_path, labels_name)
-    numeral_value = []
-    numeral_label = []
-    directories = sorted(os.listdir(numerals_path))
+    '''
+    This mthod will scan the given directory and looks for images
+    then it in turn converts it to numpy arrays as labels and values and stores them
+    '''
+    newlist = clean_data(pathinfo['labels_path'])
+    value = []
+    label = []
+    directories = sorted(os.listdir(pathinfo['numerals_path']))
     for current_dir in directories:
-        files = sorted(os.listdir("%s/%s" %
-                                  (numerals_path, current_dir)))
+        files = sorted(os.listdir("%s/%s" % (pathinfo['numerals_path'], current_dir)))
         for image in files:
-            print("reading : %s/%s/%s" %
-                  (numerals_path, current_dir, image))
-            loadedimg = opencv.imread("%s/%s/%s" %
-                                      (numerals_path, current_dir, image))
+            print("reading : %s/%s/%s" % (pathinfo['numerals_path'], current_dir, image))
+            loadedimg = opencv.imread("%s/%s/%s" % (pathinfo['numerals_path'], current_dir, image))
             img = opencv.cvtColor(loadedimg, opencv.COLOR_BGR2GRAY)
-            numeral_value.append(img)
-            numeral_label.append(int(current_dir))
-    np.save("data/numeral_value.npy", numeral_value)
-    np.save("data/numeral_label.npy", numeral_label)
+            value.append(img)
+            label.append(int(current_dir))
+    np.save("data/numeral_value.npy", value)
+    np.save("data/numeral_label.npy", label)
 
 convert_to_npy()
