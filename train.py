b34025b0f747b30a47a847447a01f43db17a048e
velansalis <velanmax8@gmail.com>
2019-04-18 08:57:47 +0530

    Thrash

--- train.py
+++ train.py
@@ -4,21 +4,19 @@ import numpy as np
 import random
 
 
-# Load Numpy and Show values
-values = np.load("numeral_value.npy")
-labels = np.load("numeral_label.npy")
-
-val = random.randint(0, len(values))
-
-print("Length of training data : %d\nLength of training labels : %d" %
-      (len(values), len(labels)))
+def get_data(values, labels):
+    values = np.load(values)
+    labels = np.load(labels)
+    return (values, labels)
 
 
 def showimageplots():
-    imglen = 4
-    figure = plt.figure()
-    for i in range(1, imglen*imglen+1):
+    values, labels = get_data("numeral_value.npy", "numeral_label.npy")
+    print("Length of training data : %d\nLength of training labels : %d" %
+          (len(values), len(labels)))
+    grid = 2
+    for i in range(1, grid*grid+1):
         index = random.randint(0, len(values))
-        plt.subplot(imglen, imglen, i).set_title(labels[index])
+        plt.subplot(grid, grid, i).set_title(labels[index])
         plt.imshow(values[index], cmap="gray", interpolation="nearest")
     plt.show()
